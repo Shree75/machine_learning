9eda0bb56af93a1ef44771f0ed6c539cb62055c6
Shree75 <shreenathkadam75@gmail.com>
2020-09-24 15:55:43 +0530

    committed

---
+++ svm.py
@@ -0,0 +1,26 @@
+import sklearn
+from sklearn import datasets
+from sklearn import svm
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+
+cancer = datasets.load_breast_cancer()
+
+# print(cancer.feature_names)
+# print(cancer.target_names)
+
+X = cancer.data
+y = cancer.target
+
+x_test, x_train, y_test, y_train = sklearn.model_selection.train_test_split(X, y, test_size = 0.2)
+
+classes = ['benign', 'malignant']
+
+clf = svm.SVC(kernel='linear', C=2)
+clf.fit(x_train, y_train)
+
+y_pred = clf.predict(x_test)
+
+acc = metrics.accuracy_score(y_test, y_pred)
+
+print(acc)
